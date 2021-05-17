fde829149cd69d6d77338db26d7dbcb33abfded4
Bilal ÇETİN <bilal.cetin.4021@gmail.com>
2021-05-17 21:03:17 +0300

    Comments Route'u Oluşturuldu Comments Listelendi

--- src/routes/posts.php
+++ src/routes/posts.php
@@ -4,6 +4,8 @@ use Psr\Http\Message\ServerRequestInterface as Request;
 
 $app = new \Slim\App;
 
+//Posts
+
 $app->get('/posts', function (Request $request, Response $response) {
 
    $db = new Db();
@@ -35,3 +37,38 @@ $app->get('/posts', function (Request $request, Response $response) {
 
    $db = null;
 });
+
+//Comments
+
+$app->get('/comments', function (Request $request, Response $response) {
+
+   $db = new Db();
+
+   try{
+      $db = $db->connect();
+      
+      $comments = $db->query("SELECT * FROM comments")->fetchAll(PDO::FETCH_OBJ);
+
+      return $response
+         ->withStatus(200)
+         ->withHeader("Content-Type", 'application/json')
+         ->withJson($comments);
+
+
+
+   }catch(PDOException $e){
+      return $response->withJson(
+         array(
+            "error" => array(
+               "text" => $e->getMessage(),
+               "code" => $e->getCode()
+            )
+         )
+      );
+   }
+
+
+
+   $db = null;
+});
+
